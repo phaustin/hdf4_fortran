84332fdab45d54d7eea14944d7cf7816433b4bda
Phil Austin <paustin@eos.ubc.ca>
2018-08-29 13:18:29 -0700

    initial

---
+++ conda.recipe/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
+export FFLAGS="$FFLAGS -std=legacy -I$PREFIX/include -L$PREFIX/lib"
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -L$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib"
+
+chmod +x configure
+
+# The --enable-silent-rules is needed because Travis CI dies on the long output from this build.
+./configure --prefix=${PREFIX}\
+            --host=$HOST \
+            --enable-linux-lfs \
+            --enable-silent-rules \
+            --with-ssl \
+            --with-zlib \
+            --with-jpeg \
+            --disable-netcdf 
+
+make
+make install
+# temporarily disabled due to segfault.
+#make check
+
+# Remove man pages.
+rm -rf ${PREFIX}/share
+
+# Avoid clashing names with netcdf.
+mv ${PREFIX}/bin/ncdump ${PREFIX}/bin/h4_ncdump
+mv ${PREFIX}/bin/ncgen ${PREFIX}/bin/h4_ncgen
+
+# People usually Google these.
+rm -rf ${PREFIX}/examples
